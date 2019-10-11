2bfce7ff21c8a77bfa38c63cf2006a50f39c9f47
Jaden <qdddyx@163.com>
2019-10-11 20:25:41 +0800

    3 dataset 5-fold cv

--- code/gbdt_ver.R
+++ code/gbdt_ver.R
@@ -7,13 +7,12 @@ library(gbm)
 library(caret)
 library(plyr)
 library(parallel)
-library(snow)
+library(stringr)
 
 # # read self-test dataset  _____5 fold cv
 
 PARS_human <- read.table(file = "../data/PARS_human/ph_encode_37.csv", header = F,sep=",",skip=1)
-PARS_yeast <- read.table(file = "../data/PARS_yeast/py_encode_37.csv", header = F,sep=",",skip=1)
-SS_PDB <- read.table(file = "../data/SS_PDB/pdb_encode_37.csv", header = F,sep=",",skip=1)
+
 
 
   ###############
@@ -32,55 +31,188 @@ holdout <- split(sample(1:N), 1:folds)
   ###############
   set.seed(666)
   print("Training the model...")
-
-  #Fold-1
-  X_train<-PARS_human[-holdout$`1`,][,c(-1,-2,-151)]
+t1<-Sys.time()
+Result<-matrix(rep(0,25),5,5)
+colnames(Result)<-c("AUC","Precision","Recall","F","Accuracy")
+for(i in 1:5){
+#Fold-1
+  X_train<-PARS_human[-holdout[[i]],][,c(-1,-2,-151)]
   #Y_trian=labels
-  Y_train<-PARS_human[-holdout$`1`,][,151]
+  Y_train<-PARS_human[-holdout[[i]],][,151]
 
-  Test_data<-PARS_human[holdout$`1`,][,c(-1,-2,-151)]
+ # Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
+ Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
 
 
+trControl<-trainControl(method = "cv",number = 3)
+gbmGrid <-  expand.grid(interaction.depth = c(5,9), 
+                        n.trees = c(400,1000), 
+                        shrinkage = c(0.03,0.1),
+                        n.minobsinnode = 10)
+#test maximization
+  gbm2=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = "pca", 
+  weights = NULL,trControl=trControl,metric = "RMSE",tuneGrid = gbmGrid)
+#original
+# trControl<-trainControl(method = "cv",number = 5)
+# gbm1=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = NULL, 
+#   weights = NULL,trControl=trControl ,metric = "RMSE",
+#   maximize = ifelse(metric %in% c("RMSE", "logLoss", "MSPE","MSAE","MAE"), FALSE,TRUE),tuneGrid = NULL)
 
-  gbm2=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = NULL, 
-  weights = NULL,trControl=trainControl(method="none")
-  ,metric = ifelse(is.factor(Y_train), "Accuracy", "RMSE"),
-  maximize = ifelse(metric %in% c("RMSE", "logLoss", "MAE"), FALSE, TRUE),tuneGrid = NULL)
+
 
   #############
   #use the model to predict
   #############
   print("Predicting scores...")
   PARS_human_result1 <- predict(gbm2, data.frame(Test_data))
-  true_class<-PARS_human[holdout$`1`,][,151]
-  class_0_prob<-1-PARS_human_result1
+  true_class<-str_glue("Class{PARS_human[holdout[[i]],][,151]}")
   class_1_prob<-PARS_human_result1
-  
-
-  ##########
-  #get the ranking
-  ##########
-  print("Give the ranking...")
-  globalRankingOfNegated <- which(sort.int(predictedWeightsGlobal, decreasing = T, index.return = T)$ix == negatedIndexInGlobalTesting)
-  localRankingOfNegated <- which(sort.int(predictedWeightsLocal, decreasing = T, index.return = T)$ix == negatedIndexInLocalTesting)
-  localRankingOfNegated2 <- which(sort.int(predictedWeightsLocal2, decreasing = T, index.return = T)$ix == negatedIndexInLocalTesting2)
-  rankings <- rbind(rankings, c(globalRankingOfNegated, localRankingOfNegated, localRankingOfNegated2))
-
-
-
-
-
-
-write.csv(rankings, file = "./test/1-100simi.csv", row.names = F)
-write.table(rankings, file = "./test/1-100simi.txt", col.names = T,row.names = F, sep = "\t")
+  test_set <- data.frame(obs = true_class,
+                         Class1 = class_1_prob)
+  test_set$Class0 <- 1 - test_set$Class1
+  test_set$pred <- factor(ifelse(test_set$Class1 >= .5, "Class1", "Class0"))
+  #get the scores
+  ACC<-confusionMatrix(data = test_set$pred, reference = test_set$obs, mode = "prec_recall")$overall["Accuracy"]
+  unitResult<-prSummary(test_set, lev = levels(test_set$obs))
+  Result[i,]<-t(as.matrix(c(unitResult,ACC)))
+}
+t2<-Sys.time()
+print("Time we need is:")
+print(t2-t1)  
+write.csv(Result, file = "../result/PARS-human-5fold.csv", row.names = F)
 
 
 
 
+############################
+#22222222222222222222222222
+############################
+t1<-Sys.time()
+PARS_human <- read.table(file = "../data/PARS_yeast/py_encode_37.csv", header = F,sep=",",skip=1)
+N <- nrow(PARS_human)
+# Number of desired splits
+folds <- 5
+# Generate indices of holdout observations
+holdout <- split(sample(1:N), 1:folds)
+  ###############
+  #Training model
+  ###############
+  set.seed(666)
+  print("Training the model...")
 
-  # gbm1=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = NULL, 
-  # weights = NULL,trControl=trainControl(method="cv",number=5,verboseIter=TRUE,returnData=TRUE,returnResamp="all")
-  # ,metric = ifelse(is.factor(Y_train), "Accuracy", "RMSE"),
-  # maximize = ifelse(metric %in% c("RMSE", "logLoss", "MAE"), FALSE,
-  # TRUE),tuneGrid = NULL)
+Result<-matrix(rep(0,25),5,5)
+colnames(Result)<-c("AUC","Precision","Recall","F","Accuracy")
+for(i in 1:5){
+#Fold-1
+  X_train<-PARS_human[-holdout[[i]],][,c(-1,-2,-151)]
+  #Y_trian=labels
+  Y_train<-PARS_human[-holdout[[i]],][,151]
 
+ # Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
+ Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
+
+
+trControl<-trainControl(method = "cv",number = 3)
+gbmGrid <-  expand.grid(interaction.depth = c(5,9), 
+                        n.trees = c(400,1000), 
+                        shrinkage = c(0.03,0.1),
+                        n.minobsinnode = 10)
+#test maximization
+  gbm2=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = "pca", 
+  weights = NULL,trControl=trControl,metric = "RMSE",tuneGrid = gbmGrid)
+#original
+# trControl<-trainControl(method = "cv",number = 5)
+# gbm1=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = NULL, 
+#   weights = NULL,trControl=trControl ,metric = "RMSE",
+#   maximize = ifelse(metric %in% c("RMSE", "logLoss", "MSPE","MSAE","MAE"), FALSE,TRUE),tuneGrid = NULL)
+
+
+
+  #############
+  #use the model to predict
+  #############
+  print("Predicting scores...")
+  PARS_human_result1 <- predict(gbm2, data.frame(Test_data))
+  true_class<-str_glue("Class{PARS_human[holdout[[i]],][,151]}")
+  class_1_prob<-PARS_human_result1
+  test_set <- data.frame(obs = true_class,
+                         Class1 = class_1_prob)
+  test_set$Class0 <- 1 - test_set$Class1
+  test_set$pred <- factor(ifelse(test_set$Class1 >= .5, "Class1", "Class0"))
+  #get the scores
+  ACC<-confusionMatrix(data = test_set$pred, reference = test_set$obs, mode = "prec_recall")$overall["Accuracy"]
+  unitResult<-prSummary(test_set, lev = levels(test_set$obs))
+  Result[i,]<-t(as.matrix(c(unitResult,ACC)))
+}
+t2<-Sys.time()
+print("Time we need is:")
+print(t2-t1)
+write.csv(Result, file = "../result/PARS-yeast-5fold.csv", row.names = F)
+
+
+
+
+############################
+#3333333333333333333333333
+############################
+t1<-Sys.time()
+PARS_human <- read.table(file = "../data/SS_PDB/pdb_encode_37.csv", header = F,sep=",",skip=1)
+N <- nrow(PARS_human)
+# Number of desired splits
+folds <- 5
+# Generate indices of holdout observations
+holdout <- split(sample(1:N), 1:folds)
+  ###############
+  #Training model
+  ###############
+  set.seed(666)
+  print("Training the model...")
+
+Result<-matrix(rep(0,25),5,5)
+colnames(Result)<-c("AUC","Precision","Recall","F","Accuracy")
+for(i in 1:5){
+#Fold-1
+  X_train<-PARS_human[-holdout[[i]],][,c(-1,-2,-151)]
+  #Y_trian=labels
+  Y_train<-PARS_human[-holdout[[i]],][,151]
+
+ # Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
+ Test_data<-PARS_human[holdout[[i]],][,c(-1,-2,-151)]
+
+
+trControl<-trainControl(method = "cv",number = 3)
+gbmGrid <-  expand.grid(interaction.depth = c(5,9), 
+                        n.trees = c(400,1000), 
+                        shrinkage = c(0.03,0.1),
+                        n.minobsinnode = 10)
+#test maximization
+  gbm2=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = "pca", 
+  weights = NULL,trControl=trControl,metric = "RMSE",tuneGrid = gbmGrid)
+#original
+# trControl<-trainControl(method = "cv",number = 5)
+# gbm1=caret::train(data.frame(X_train), Y_train, method = "gbm", preProcess = NULL, 
+#   weights = NULL,trControl=trControl ,metric = "RMSE",
+#   maximize = ifelse(metric %in% c("RMSE", "logLoss", "MSPE","MSAE","MAE"), FALSE,TRUE),tuneGrid = NULL)
+
+
+
+  #############
+  #use the model to predict
+  #############
+  print("Predicting scores...")
+  PARS_human_result1 <- predict(gbm2, data.frame(Test_data))
+  true_class<-str_glue("Class{PARS_human[holdout[[i]],][,151]}")
+  class_1_prob<-PARS_human_result1
+  test_set <- data.frame(obs = true_class,
+                         Class1 = class_1_prob)
+  test_set$Class0 <- 1 - test_set$Class1
+  test_set$pred <- factor(ifelse(test_set$Class1 >= .5, "Class1", "Class0"))
+  #get the scores
+  ACC<-confusionMatrix(data = test_set$pred, reference = test_set$obs, mode = "prec_recall")$overall["Accuracy"]
+  unitResult<-prSummary(test_set, lev = levels(test_set$obs))
+  Result[i,]<-t(as.matrix(c(unitResult,ACC)))
+}
+t2<-Sys.time()
+print("Time we need is:")
+print(t2-t1)
+write.csv(Result, file = "../result/SS_PDB-5fold.csv", row.names = F)
